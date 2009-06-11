e0b6e289836d48520d464fb11f1448abf2412ad7
Scott Tesoriere <scott@keepflippin.com>
2009-06-10 22:44:57 -0400

    added everything

---
+++ assets/locking.js
@@ -0,0 +1,137 @@
+var Locking;
+var lockingBox;
+(function($) {
+	/* thank you github-voice :) */
+
+	lockingBox = {
+
+		Options: {
+			html: '\
+	    <div id="locking-wrapper"> \
+	      <h1>Alert!</h1> \
+	      <div id="locking"> \
+	        <p class="description"></p> \
+	        <p class="call-to-action"><a href="#"></a></p> \
+	      </div> \
+	    </div>',
+			text: {
+				description: "The lock on this entry just expired!",
+	      callToAction: "&raquo; Renew your lease"
+			}
+	
+		},
+
+		init: function(msg) {
+			$('body')
+        .append('<div id="locking-overlay"></div>')
+        .find('#locking-overlay')
+          .css({
+            width   : $(window).width(),
+            height  : $(document).height(),
+            opacity : 0.75
+          });
+					
+		  if (msg == null) {
+				msg = this.Options.text.description;
+			}
+      $('body')
+        .append(this.Options.html)
+        .find('#locking')
+          .find('p.description').html(msg).end()
+          .find('p.call-to-action a')
+            .html(this.Options.text.callToAction)
+						.attr('onclick', "javascript:Locking.forceRenew('"+Locking.Data.entry_id+"','"+Locking.Data.author_id+"');")
+						.attr('href', "javascript:Locking.forceRenew('"+Locking.Data.entry_id+"','"+Locking.Data.author_id+"');").end();
+            // .attr('href', '#');
+
+			this.updatePosition();
+		},
+		
+		updateMessage: function(msg) {
+			
+			if ($('#locking-overlay').length <= 0)
+				this.init();
+			$('#locking > p.description').html(msg).end();
+			
+		},
+		updatePosition: function() {
+	    $('#locking-wrapper').css('margin-top', -1 * ($('#locking-wrapper').height() / 2));
+	  }
+
+		
+	},
+	
+	/* thank you nick dunn ;p */
+	Locking = {
+
+    URL: {
+      root: null,
+      symphony_root: null,
+    },
+
+		Data: {
+			entry_id: null,
+			author_id: null
+		},
+	
+		init: function() {
+			
+			var h1 = $('h1:first');
+			var root = h1.find('a').attr('href');
+      var url = window.location.href.replace(root, '').split('/');
+      
+      this.URL.root = root;
+      this.URL.symphony_root = this.URL.root + 'symphony/';
+      this.URL.page = url[1];
+
+		},
+
+		disableForm: function() {
+			$("button").attr("disabled", true);
+			$("input").attr("disabled", true);
+			$("textarea").attr("disabled", true);
+		},
+			
+		renewLock: function(entry_id, author_id, time) {
+			var url = this.URL.symphony_root;
+			this.Data.entry_id = entry_id;
+			this.Data.author_id = author_id;
+			interval = setInterval(function() {
+				data = "entry_id="+entry_id+"&author_id="+author_id;
+	      $.post(url + 'extension/pessimistic_db_locking/ajax_locking/', data, function(response){
+					if (response == '"expired"') {
+						lockingBox.updateMessage("The lock on this entry doesn't exist (did you leave this window open?)");
+					}
+					else if (response == '"expired-lifetime"') {
+						lockingBox.updateMessage('Your lock expired (did you leave this window open?)');
+					}
+					else if (response == '"true"'){
+						// we renewed
+					}
+					// someone else owns it
+					else {
+						lockingBox.updateMessage(response+' owns the lease now!');
+					}
+	        console.log(response);
+	      });				
+			}, time*1000);
+		},
+		
+		forceRenew: function(entry_id, author_id, time) {
+			data = "entry_id="+entry_id+"&author_id="+author_id+"&force=true";
+      $('#locking-overlay, #locking-wrapper').remove();
+			$.post(this.URL.symphony_root + 'extension/pessimistic_db_locking/ajax_locking/', data, function(response) {
+			})			
+		}
+	
+		
+	}
+	
+})(jQuery.noConflict());
+
+jQuery(document).ready(function() {
+	Locking.init();
+	
+
+		
+});
---
+++ content/content.ajax_locking.php
@@ -0,0 +1,50 @@
+<?php
+
+	require_once(TOOLKIT . '/class.administrationpage.php');
+	require_once(TOOLKIT . '/class.entrymanager.php');
+	require_once(TOOLKIT . '/class.authormanager.php');
+	
+	class ContentExtensionPessimistic_DB_LockingAjax_Locking extends AdministrationPage {
+		protected $_driver = null;
+		
+		public function __construct(&$parent){
+			parent::__construct($parent);
+			$this->_driver = $this->_Parent->ExtensionManager->create('pessimistic_db_locking');
+		}
+		
+		public function __viewIndex() {
+			
+			
+			$entry_id = $_REQUEST['entry_id'];
+			$author_id = $_REQUEST['author_id'];
+			
+			$force = $_REQUEST['force'];
+			if ($force == 'true') {
+				$this->_driver->removeTheLockByEntry($entry_id);
+				$this->_driver->renewTheLock($entry_id, $author_id);
+				echo json_encode('true');
+				exit;
+			}
+			
+			$lock = $this->_driver->lockExists($entry_id);
+			if ($author_id != $lock[0] && $lock[0] > 0) {
+
+		    $authorManager = new AuthorManager($this->_Parent);
+				$author = $authorManager->fetchByID($lock[0]);
+				echo json_encode($author->getFullName());
+			}
+			else if ($lock == -1) {
+				echo json_encode('expired-lifetime');
+			}
+			else if ($lock == 0) {
+				echo json_encode('expired');
+			}
+			else {
+				$this->_driver->renewTheLock($entry_id, $author_id);
+				echo json_encode('true');
+			}
+			
+			exit;	
+
+		}
+	}
